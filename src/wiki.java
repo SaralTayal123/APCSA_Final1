a192c52f70dc1761b2a95bf8b23ae662cbf238da
Saral Tayal <thunderbird211@gmail.com>
2017-03-23 21:44:20 +0530

    URL Input Stream for wikipedia

--- src/wiki.java
+++ src/wiki.java
@@ -1,29 +1,15 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.BufferedInputStream;
 import java.net.URL;
+
+/**
+ * created by Saral Tayal on 23/3/17
+ */
 
 public class wiki {
 
-    String subject = "Ed Sheeran";
-    URL url = new URL("https://en.wikipedia.org/w/index.php?action=raw&title=" + subject.replace(" ", "_"));
-    String text = "";
-try (BufferedReader br = new BufferedReader(new InputStreamReader(url.openConnection().getInputStream()))) {
-        String line = null;
-        while (null != (line = br.readLine())) {
-            line = line.trim();
-            if (!line.startsWith("|")
-                    && !line.startsWith("{")
-                    && !line.startsWith("}")
-                    && !line.startsWith("<center>")
-                    && !line.startsWith("---")) {
-                text += line;
-            }
-            if (text.length() > 200) {
-                break;
-            }
-        }
+    public void getWiki() {
+
     }
-System.out.println("text = " + text);
 }
 
 
